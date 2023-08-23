011fcc3a288484f1bd9e614d82dd55166dd055c3
Omar <github@omaraziz.dev>
2023-08-23 08:47:11 -0700

    check against allowed origins

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,29 @@ import { isPossiblyCID, isURL } from './utilities.ts'
 const IPFS_GATEWAY_HOST = Deno.env.get('IPFS_GATEWAY_HOST')
 if (!IPFS_GATEWAY_HOST) throw new Error('IPFS_GATEWAY_HOST environment variable not set')
 
-async function handler(request: Request) {
+async function handler(request: Request, info: Deno.ServeHandlerInfo): Promise<Response> {
+  const headers = new Headers(request.headers)
+  headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS, HEAD')
+  headers.set('Access-Control-Allow-Headers', 'Content-Type')
+  
+  const { hostname: identifier } = info.remoteAddr ?? 'anonymous'
+
+  const origin = headers.get('Origin')
+  const allowedOrigins = Deno.env.get('ALLOWED_ORIGINS')?.split(',') ?? []
+
+  if (!allowedOrigins.includes(identifier)) {
+    return new Response(`Origin ${origin} not allowed`, { status: 403, headers })
+  }
+
+  if (request.method === 'OPTIONS') {
+    return new Response(null, { status: 204, headers })
+  }
   const { pathname, search } = new URL(request.url)
 
-  if (pathname.length === 0) return new Response('No path provided', { status: 400 })
-  if (pathname === '/favicon.ico') return new Response('', { status: 418 })
+  if (pathname.length === 0) {
+    return new Response('No path provided', { status: 400, headers })
+  }
+  if (pathname === '/favicon.ico') return new Response('', { status: 418, headers })
 
   const pathParameters = pathname.split('/').filter(Boolean)
   // only `/ipfs/<cid>` or `/<cid>` allowed for pathname
@@ -17,12 +35,16 @@ async function handler(request: Request) {
     ? pathParameters.at(0)
     : undefined
 
-  if (!isPossiblyCID(cid)) return new Response(`Invalid pathname: ${pathname}`, { status: 400 })
+  if (!isPossiblyCID(cid)) {
+    return new Response(`Invalid pathname: ${pathname}`, { status: 400, headers })
+  }
 
   const ipfsURL = `${IPFS_GATEWAY_HOST}/ipfs/${cid}` + search
-  if (!isURL(ipfsURL)) return new Response(`Invalid IPFS URL: ${ipfsURL}`, { status: 400 })
+  if (!isURL(ipfsURL)) {
+    return new Response(`Invalid IPFS URL: ${ipfsURL}`, { status: 400, headers })
+  }
 
-  return fetch(ipfsURL)
+  return fetch(ipfsURL, { headers })
 }
 
 Deno.serve(
